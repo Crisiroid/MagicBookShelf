f170281d2deed0e7649c68c1455a79b41a62cf31
Crisiroid <csiroid@gmail.com>
2024-02-22 11:02:29 +0330

    Book Data Model Created

--- server/server/Model/Book.cs
+++ server/server/Model/Book.cs
@@ -1,6 +1,18 @@
-﻿namespace server.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace server.Model
 {
     public class Book
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string ImageUrl { get; set; }
+        public string UID { get; set; }
+
     }
 }
--- server/server/Model/DTO/BookDTO.cs
+++ server/server/Model/DTO/BookDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace server.Model.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Model.DTO
 {
     public class BookDTO
     {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public string UID { get; set; }
     }
 }
